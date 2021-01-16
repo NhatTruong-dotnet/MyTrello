1a1f8ecd80cd75f724f33f8a4e248c8e2719185a
NhatTruong-dotnet <55442462+NhatTruong-dotnet@users.noreply.github.com>
2021-01-16 22:23:44 +0700

    Add Item

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
 
 // Items
-let updatedOnLoad = false;
+var updatedOnLoad = false;
 
 
 // Initialize Arrays
@@ -87,8 +87,30 @@ function updateDOM() {
   updatedOnLoad = true;
   updateSavedColumns();
 }
+// Add to Column List, Reset TextBox
+function addToColumn(column){
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
 
-// Allows arrays to reflect Drag and Drop items
+// Show Add Item Input Box
+function showInputBox(column){
+  addBtns[column].style.visibility ='hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display='flex';
+}
+
+// Hide Item Input Box
+function hideInputBox(column){
+  addBtns[column].style.visibility ='visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display='none';
+  addToColumn(column);
+}
+// Allows arrays to reflect Drag and Drop items  
 function reBuildArrays(){
   backlogListArray = [];
   for (let index = 0; index < backlogList.children.length; index++) {
